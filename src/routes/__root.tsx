448fe0dbe09bd76691de529f7669cac017ab35ff
Caio Marques Silva <caiocpe@gmail.com>
2026-02-16 16:19:54 -0300

    theming

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ThemeProvider } from '@/components/theme-provider'
+import '../index.css'
 
 const RootLayout = () => (
-  <>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+  </ThemeProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
