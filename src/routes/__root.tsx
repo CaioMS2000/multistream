a960014b1f8e3ba2765a75ff8f0e21b9543aa685
Caio Marques Silva <caiocpe@gmail.com>
2026-02-17 15:15:15 -0300

    feat: add Card and Toaster UI components and integrate Toaster in root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,15 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
 import '../index.css'
 
 const RootLayout = () => (
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <Outlet />
-    <TanStackRouterDevtools />
-  </ThemeProvider>
+	<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+		<Outlet />
+		<Toaster />
+		<TanStackRouterDevtools />
+	</ThemeProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
