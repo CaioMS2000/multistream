36c18afe0e1e92792c319f7f4040320fafd9f3cd
Caio Marques Silva <caiocpe@gmail.com>
2026-02-18 14:54:05 -0300

    chore(players): comment out debug channel label in kick and twitch players

--- src/components/players/kick-player.tsx
+++ src/components/players/kick-player.tsx
@@ -6,7 +6,7 @@ export function KickPlayer({ channel }: KickPlayerProps) {
 	return (
 		<>
 			<div className="bg-green-500 w-full h-full">
-				<span className="text-white">{channel}</span>
+				{/* <span className="text-white">{channel}</span> */}
 			</div>
 		</>
 	)
--- src/components/players/twitch-player.tsx
+++ src/components/players/twitch-player.tsx
@@ -6,7 +6,7 @@ export function TwitchPlayer({ channel }: TwitchPlayerProps) {
 	return (
 		<>
 			<div className="bg-purple-500 w-full h-full">
-				<span className="text-white">{channel}</span>
+				{/* <span className="text-white">{channel}</span> */}
 			</div>
 		</>
 	)
